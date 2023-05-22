f3f2f36f3a6b5c032fd8bb792c20a01ededd0a7c
Mohammad Shirazi <112489298+mmdtcn@users.noreply.github.com>
2023-05-22 14:46:20 -0400

    Create main.py

---
+++ Tutorial/Lesson01/main.py
@@ -0,0 +1,34 @@
+# Create PyGame Screen with background image, icon and exit event
+import pygame
+
+running = True
+
+background_color = (255, 255, 255)
+pygame.display.set_caption("Lesson 1: Display Window")
+
+# Screen size setup
+screen_size = (800, 600)
+screen = pygame.display.set_mode(screen_size)
+
+# Read an image
+bg_img = pygame.image.load('background.jpg')
+
+# Resize the image to screen size as background
+background_img = pygame.transform.scale(bg_img, screen_size)
+
+# Read an image
+icon_img = pygame.image.load('icon.png')
+
+# set the icon
+pygame.display.set_icon(icon_img)
+
+# display screen with quit button
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill(background_color)
+    screen.blit(background_img,(0,0))
+    pygame.display.update()
+
+quit()
