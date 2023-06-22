88110733447a5a66fb09a11fe160fc3e0cbddd34
Paul Talaga <talagap@uindy.edu>
2023-06-22 14:46:23 -0400

    typing game with networking

---
+++ TypeSpeed/run-scoreboard.py
@@ -0,0 +1,45 @@
+import random
+import time
+import socket
+import pickle
+
+port = 5555
+ip = "10.15.102.49"
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+words = ['the','camp','shop','computer','metal','wood']
+words = ['the','camp']
+
+#print(words)
+
+random.shuffle(words)
+
+#print(words)
+
+paragraph = " ".join(words)
+
+#print(paragraph)
+ready = input("Ready to go?")
+
+print(paragraph)
+start_time = time.time()
+
+response = input("Type the words above: ")
+end_time = time.time()
+
+message = ""
+if response == paragraph:
+	print("Correct!")
+	elapsed = end_time - start_time
+	print(f"It took you {elapsed} seconds!")
+	print(f"That is {len(words) / elapsed * 60} words per minute!")
+	#message = f"Paul WPM:{len(words) / elapsed * 60}"
+	package = {'name':'Paul2', 'score':len(words) / elapsed * 60}
+	message = pickle.dumps(package)
+else:
+	print("Incorrect typing")
+	message = "Paul needs more work".encode('ascii')
+
+#s.sendto(message.encode('ascii'), (ip, port))
+s.sendto(message, (ip, port))
